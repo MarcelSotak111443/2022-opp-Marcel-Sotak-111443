785984e85827a301955c8d34c0d4150acd1e1682
Marcel_Sotak <marcel.sotak123@gmail.com>
2022-06-10 14:42:29 +0200

    first commit

---
+++ codes/prog2/Sotak_Marcel_111443_zadanie02/cmake-build-debug/CMakeFiles/Sotak_Marcel_111443_zadanie02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sotak_Marcel_111443_zadanie02.dir/main.cpp.obj"
+  "Sotak_Marcel_111443_zadanie02.exe"
+  "Sotak_Marcel_111443_zadanie02.exe.manifest"
+  "Sotak_Marcel_111443_zadanie02.pdb"
+  "libSotak_Marcel_111443_zadanie02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sotak_Marcel_111443_zadanie02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
